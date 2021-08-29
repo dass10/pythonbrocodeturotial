71e8e2d289266b6d218da571263680ff0fd734b2
shweta <dass10@myunitec.ac.nz>
2021-08-29 23:48:46 +1200

    Initial commit

---
+++ variable.py
@@ -0,0 +1,28 @@
+# variable = a container for a value,Behaves as the value that it contains
+# String is a series of charecters,so to create a string we either use a single quote or double quotes,and we can assigna value of whatever your own name is.
+# Now a common naming convention with variables if it has two words separate each word with underscore,it's not necessary but it is a common practice for python.
+first_name = "Shweta"
+last_name = "Das"
+full_name = first_name +" "+ last_name
+# when you print make sure you do not put quote because what we doing then is literally print the word name as
+# so that is how to check the data charector.just put (type) to see the class.
+#print(type(name))
+print("Hello "+full_name)
+#with string you can not add any sort of math.
+#Integer data type or int-when you assign a value of int data type make sure that it is not within the quotes because then technically it would be a string then and you will that the color shceme for this number actually changed.
+age = 40
+age +=  1
+print("your age is: "+str(age))
+#print(type(age)) the data type the age variable it is int
+
+#Float Data type is a numeric that can store number that includes a decimal portion.an int data type cannot store a decimal portion that is why it is
+height = 167.5
+print("your height is: "+str(height)+"cm")
+#print(type(height))
+
+#Boolean(True or false)
+human = True
+print(human)
+#print(type(human))
+# so end of the basic,things to remember that there 4 basic data types,string which stores aseries of charecter,int which store a whole number,Float are floating point numbers a numeric value with decimal and Booleans which only stores true or false and they are very useful with if statement.finished at 17:28
+
